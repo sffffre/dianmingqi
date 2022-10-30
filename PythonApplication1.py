d172ee17d8cc43dcf397302d7d088f728d48f0b2
sffffre <91270594+sffffre@users.noreply.github.com>
2022-10-30 11:49:59 +0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -11,22 +11,37 @@ stu_list = ['stu1','stu2','stu3','stu4','stu5','stu6',
                'stu36','stu37','stu38','stu39','stu40','stu41',
 	'stu42','stu43','stu44','stu45','stu46',
 	'stu47','stu48','stu49','stu50']
+re_list = []
+new_list = []
 
-stu = '开始点名'
-last = []
 
 root = tk.Tk()
 root.geometry("900x900")
 root.title('点名器')
 root.resizable(False,False)
 
-label = tk.Label(root, text=stu,font=('黑体',35),width=30,height=5)
+stu = '开始点名'
+last = []
+text = tk.StringVar()
+text.set(stu)
+
+label = tk.Label(root, text=stu,font=('黑体',35),width=30,height=5,textvariable=text)
 label.place(x=150,y=300)
 
 def callback():
-    stu=random.choice(stu_list)
+    while True:
+        stu=random.choice(stu_list)
+        if stu in re_list:
+            pass
+        else:
+            re_list.append(stu)
+            if len(re_list) > 5:
+                del re_list[0]
+            break
+    
+    text.set(stu)
+
 b = tk.Button(root, text="点名", command=callback, width=20,height=10)
 b.place(x=300,y=750)
 
 root.mainloop()
-
